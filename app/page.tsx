ed54e761dd249fb797236fc56872ed844ee23a8e
gohardorgohome1 <gohardorgohome1@github.com>
2025-03-14 16:56:54 +0100

    check sonar again

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
+"use client"; // Foor components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 import "@ant-design/v5-patch-for-react-19";
 import { useRouter } from "next/navigation";
 import { Button } from "antd";
