e0a925e2d5849ec5865e6f49b67e8c608ac62e2a
PascalSenn2 <pascal.senn2@uzh.ch>
2025-04-01 07:57:10 +0200

    Registration page fully designed #38 (#63)
    * Changed design of registration page partly: #38
    
    * Added text and some design changes in registration page #38
    
    * Added some styling in input Card (Registration) #38
    
    * Formatted Register and Log in button #38
    
    * Changed background image - now in globals.css #38
    
    * Modified text color & gradients and added title image #38
    
    * Changed shadow for some text (registration) #38
    
    * Modified layout & content of card #38
    
    * Adjusted size of card & text #38

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation"; // use NextJS router for navigation
 import { useApi } from "@/hooks/useApi";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { User } from "@/types/user";
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Card } from "antd";
 // Optionally, you can import a CSS module or file for additional styling:
 // import styles from "@/styles/page.module.css";
 
@@ -26,8 +26,6 @@ const Register: React.FC = () => {
           if (response.id != null){
             localStorage.setItem("userId", response.id.toString());
           }
-          
-           // Redirect to users overview after registration
         }
         
       } catch (error) {
@@ -48,48 +46,278 @@ const Register: React.FC = () => {
         }
       }
 
-      // Navigate to the user overview
-      router.push("/users");
+      // Navigate to the dashboard after successfull registration
+      router.push("/dashboard");
       
     };
-  
+
     return (
-      <div className="register-container">
-        <Form
+      
+      <div
+        className="register-container"
+        style={{
+          position: "relative",
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column", // Makes sure the title image (Exoplanet Hunting Platform) stays above the Card
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img // Title image: "Exoplanet Hunting Platform"
+          style={{
+            width: "80vw",
+            height: "14vh",
+            marginTop: "50px" // determines distance to the top of the window
+            }}
+          src="/title.png" // image under /public/title.png
+        />
+
+        <Card // Card for registration
+        className="dashboard-container"
+        style={{
+          padding: "20px", // position & size
+          paddingTop: "50px", // adjust to center vertically
+          height: "400px",
+          width: "620px",
+
+          background: "black", // visuals
+          border: "none",
+          borderRadius: 98,
+          backgroundColor: "rgba(0, 0, 0, 0.65)", // instead of opacity = 0.65 -> buttons etc. would inherit opacity
+
+          zIndex: 1 // foreground
+        }}
+      >
+        <Form // Registration
           form={form}
           name="register"
           size="large"
           variant="outlined"
           onFinish={handleRegister}
-          layout="vertical"
+          layout="horizontal" // Label to the left by default
+          colon={false} // no colon between label and input field
+          requiredMark={false} // no star before label
         >
-          <Form.Item
+          <Form.Item // Input username field & Label
             name="username"
-            label="Username"
+            label={<span
+              style={{
+                width: "317px", // size
+                height: "49px",
+
+                textAlign: "end", // font & text related
+                background: "linear-gradient(90deg, #8A5555, #FFFFFF)", // color gradient
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                fontSize: "40px",
+                fontFamily: "Karantina", // imported fontFamily -> see top of globals.css
+                fontWeight: "700",
+                WebkitTextStrokeWidth: "1px",   // Add black edge to text
+                WebkitTextStrokeColor: "#000000",
+              }}
+            >
+              Username
+            </span>
+            }
+            labelCol={{ span: 8 }} // "maximum" width of the label
+            wrapperCol={{ span: 18 }} // "maximum" width of the input field
+
             rules={[{ required: true, message: "Please input your username!" }]}
           >
-            <Input placeholder="Enter a username" />
+            <Input
+            style={{
+              width: "340px",
+              height: "52px",
+              background: "white",
+              borderRadius: 46,
+              fontSize: "24px",
+              fontFamily: "Jura",
+              color: "#000000"
+            }}
+            />
           </Form.Item>
-          <Form.Item
-          name="name"
-          label="name"
-          rules={[{ required: false, message: "optional" }]}
-        >
-          <Input placeholder="optional" />
-        </Form.Item>
-          <Form.Item
+
+          <Form.Item // Input password field & Label
             name="password"
-            label="Password"
+            label={<span
+              style={{
+                width: "317px", // size
+                height: "49px",
+
+                textAlign: "end", // font & text related
+                background: "linear-gradient(90deg, #8A5555, #FFFFFF)", // color gradient
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                fontSize: "40px",
+                fontFamily: "Karantina", // imported fontFamily -> see top of globals.css
+                fontWeight: "700",
+                WebkitTextStrokeWidth: "1px",   // Add black edge to text
+                WebkitTextStrokeColor: "#000000",
+              }}
+            >
+              Password
+            </span>
+            }
+            labelCol={{ span: 8 }} // "maximum" width of the label
+            wrapperCol={{ span: 18 }} // "maximum" width of the input field
+
             rules={[{ required: true, message: "Please input a password!" }]}
           >
-            <Input.Password placeholder="Create password" />
+            <Input.Password
+            style={{
+              width: "340px",
+              height: "52px",
+              background: "white",
+              borderRadius: 46,
+              fontSize: "24px",
+              color: "#000000"
+            }}
+            />
           </Form.Item>
+
           <Form.Item>
-            <Button type="primary" htmlType="submit" className="register-button">
-              Register
-            </Button>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: "10px"
+              }}>
+              <Button // Register button
+                type="primary"
+                htmlType="submit"
+                className="register-button"
+                style={{
+                  width: "509px", // button size & style
+                  height: "55px",
+                  background: "black",
+                  borderRadius: 46,
+
+                  textAlign: "center", // Text size & style
+                  color: "#8A5555",
+                  fontSize: "40px",
+                  fontFamily: "Karantina", // imported fontFamily -> see top of globals.css
+                  fontWeight: "700",
+
+                  boxShadow: "none", // removes default green shadow of button
+                }}
+              >
+              <span
+              style={{
+                background: "linear-gradient(90deg, #8A5555, #FFFFFF)", // color gradient
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                WebkitTextStrokeWidth: "1px",   // Add black edge to text
+                WebkitTextStrokeColor: "#000000",
+              }}
+              >
+                REGISTER
+              </span>
+              </Button>
+              <div
+                style={{
+                  display: "flex", 
+                  alignItems: "center", 
+                  justifyContent: "center", 
+                  gap: "10px"
+                }}>
+                <span
+                  style=
+                  {{
+                    textAlign: "center",
+                    color: "#8A5555",
+                    fontSize: "24px",
+                    fontFamily: "Jura", // imported fontFamily -> see top of globals.css
+                    fontWeight: "700",
+
+                    background: "linear-gradient(90deg, #8A5555, #FFFFFF)", // color gradient
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                  }}
+                  >
+                    Already registered? Then log in
+                  </span>
+                <Button // Login button
+                  onClick={() => router.push("/login")}
+                  type="primary"
+                  htmlType="button"
+                  style={{
+                    width: "100px", // button size & style
+                    height: "35px",
+                    background: "black",
+                    borderRadius: 46,
+
+                    textAlign: "center", // Text size & style
+                    color: "#8A5555",
+                    fontSize: "20px",
+                    fontFamily: "Karantina", // imported fontFamily -> see top of globals.css
+                    fontWeight: "700",
+
+                    boxShadow: "none", // removes default green shadow of button
+                  }}
+                >
+                <span
+                  style={{
+                    background: "linear-gradient(90deg, #FFFFFF, #8A5555)", // color gradient
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                  }}
+                  >
+                  Log in
+                </span>
+                </Button>
+                </div>
+              </div>
           </Form.Item>
         </Form>
+        </Card>
+        <div
+          style={{
+            position: "relative", // position & size
+            width: "800px",
+            height: "168px",
+            marginTop: "40px",
+            marginBottom: "20px",
+            textAlign: "center",
+          }}
+        >
+          <h1 // Text Exoplanet Hunting Platform
+            style={{
+              position: "relative",
+              fontSize: "32px",
+              fontFamily: "Jura", // imported fontFamily -> see top of globals.css
+              fontWeight: "700",
+              wordWrap: "break-word",
+
+              background: "linear-gradient(90deg, #000000, #B60000)", // color gradient
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+
+              zIndex: 1, // foreground
+            }}
+          >
+            Take exoplanet collaborative research to the next level!
+          </h1>
+          <h1 // Shadow Exoplanet Hunting Platform
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              fontFamily: "Jura",
+              fontSize: "32px",
+              fontWeight: "700",
+              wordWrap: "break-word",
+              color: "transparent", // transparent text
+              textShadow: "0px 4px 10.8px rgba(255, 255, 255, 1)", // White shadow behind text
+                    // adjust this ^ to change strength of shadow
+              zIndex: 0,
+            }}
+          >
+            Take exoplanet collaborative research to the next level!
+          </h1>
+        </div>
       </div>
     );
   };
