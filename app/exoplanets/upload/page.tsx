c18f6852d1655b5ec330305ce72b17c7d9f69ead
PascalSenn2 <pascal.senn2@uzh.ch>
2025-04-02 17:45:54 +0200

    Implemented Card and page title #22

--- app/exoplanets/upload/page.tsx
+++ app/exoplanets/upload/page.tsx
@@ -2,116 +2,58 @@
 import "@ant-design/v5-patch-for-react-19";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { Button } from "antd";
-import { BookOutlined, CodeOutlined, GlobalOutlined } from "@ant-design/icons";
-import styles from "@/styles/page.module.css";
+import { Button, Form, Input, Card } from "antd";
 
 export default function Home() {
   const router = useRouter();
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            <code>app/page.tsx</code> is the landing page for your application, currently being displayed.
-          </li>
-          <li>
-            <code>app/login/page.tsx</code> is the login page for users.
-          </li>
-          <li>
-            <code>app/users/page.tsx</code> is the dashboard that shows an overview of all users, fetched from the server.
-          </li>
-          <li>
-            <code>app/users/[id]/page.tsx</code> is a slug page that shows info of a particular user. Since each user has its own id, each user has its own infopage, dynamically with the use of slugs.
-          </li>
-          <li>
-            To test, modify the current page <code>app/page.tsx</code> and save to see your changes instantly.
-          </li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <Button
-            type="primary" // as defined in the ConfigProvider in [layout.tsx](./layout.tsx), all primary antd elements are colored #22426b, with buttons #75bd9d as override
-            color="red" // if a single/specific antd component needs yet a different color, it can be explicitly overridden in the component as shown here
-            variant="solid" // read more about the antd button and its options here: https://ant.design/components/button
-            onClick={() =>
-              globalThis.open(
-                "https://vercel.com/new",
-                "_blank",
-                "noopener,noreferrer",
-              )}
-            target="_blank"
-            rel="noopener noreferrer"
+  return (
+    <div
+        className="exoplanet-background"
+        style={{
+          position: "relative",
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+    >
+
+      <Card // Card for upload
+        className="exoplanet-container"
+        style={{
+          width: "95vw", // size
+          height: "95vh",
+
+          background: "black", // visuals
+          border: "none",
+          borderRadius: 98,
+          backgroundColor: "rgba(0, 0, 0, 0.66)", // instead of opacity = 0.66 -> buttons etc. would inherit opacity
+
+          zIndex: 1 // foreground
+        }}
+      >
+        <div // Title: Exoplanet Transit Analysis
+          style={{
+            width: "50vw",
+            height: "12vh",
+            textAlign: 'center',
+            color: '#FFD9D9',
+            fontSize: 72,
+            fontFamily: 'Koulen',
+            fontWeight: '400',
+
+            background: "linear-gradient(90deg, #FFD9D9, #73CBC9)", // color gradient
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            }}
           >
-            Deploy now
-          </Button>
-          <Button
-            type="default"
-            variant="solid"
-            onClick={() =>
-              globalThis.open(
-                "https://nextjs.org/docs",
-                "_blank",
-                "noopener,noreferrer",
-              )}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </Button>
-          <Button
-            type="primary"
-            variant="solid"
-            onClick={() => router.push("/login")}
-          >
-            Go to login
-          </Button>
-          <Button
-            type="primary"
-            variant="solid"
-            onClick={() => router.push("/register")}
-          >
-            Register
-          </Button>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <Button
-          type="link"
-          icon={<BookOutlined />}
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </Button>
-        <Button
-          type="link"
-          icon={<CodeOutlined />}
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Examples
-        </Button>
-        <Button
-          type="link"
-          icon={<GlobalOutlined />}
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to nextjs.org →
-        </Button>
-      </footer>
+            Exoplanet Transit Analysis
+          </div>
+      </Card>
+
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@
 import "@ant-design/v5-patch-for-react-19";
 import { useRouter } from "next/navigation";
 import { Button } from "antd";
-// import { BookOutlined, CodeOutlined, GlobalOutlined } from "@ant-design/icons";
-import styles from "@/styles/page.module.css";
-
 
 export default function Home() {
   const router = useRouter();
