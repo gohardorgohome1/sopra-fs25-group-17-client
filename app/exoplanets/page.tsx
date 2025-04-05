8628c6bc54ac336756b4ecce0b0861b4d63e6410
PascalSenn2 <pascal.senn2@uzh.ch>
2025-04-05 16:10:28 +0200

    Users cannot access /dashboard anymore -> automatic/manual redirection

--- app/exoplanets/page.tsx
+++ app/exoplanets/page.tsx
@@ -1,129 +1,29 @@
-// this code is part of S2 to display a list of all registered users
-// clicking on a user in this list will display /app/users/[id]/page.tsx
 "use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect} from "react";
 import { useRouter } from "next/navigation";
-import { useApi } from "@/hooks/useApi";
-import useLocalStorage from "@/hooks/useLocalStorage";
-import { User } from "@/types/user";
-import { Button, Card, Table } from "antd";
-import type { TableProps } from "antd"; // antd component library allows imports of types
-// Optionally, you can import a CSS module or file for additional styling:
-// import styles from "@/styles/page.module.css";
-// import "@/styles/views/Dashboard.scss";
+import { Button} from "antd";
 
-// Columns for the antd table of User objects
-const columns: TableProps<User>["columns"] = [
-  {
-    title: "Username",
-    dataIndex: "username",
-    key: "username",
-  },
-  /*{
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Id",
-    dataIndex: "id",
-    key: "id",
-  },*/
-];
-
-const Dashboard: React.FC = () => {
+const ExoplanetNonExistant: React.FC = () => {
   const router = useRouter();
-  const apiService = useApi();
-  const [users, setUsers] = useState<User[] | null>(null);
-  // useLocalStorage hook example use
-  // The hook returns an object with the value and two functions
-  // Simply choose what you need from the hook:
-  const {
-    value: token, // is commented out because we dont need to know the token value for logout
-    // set: setToken, // is commented out because we dont need to set or update the token value
-    clear: clearToken, // all we need in this scenario is a method to clear the token
-  } = useLocalStorage<string>("token", ""); // if you wanted to select a different token, i.e "lobby", useLocalStorage<string>("lobby", "");
-
-  const handleLogout = async () : Promise<void> => {
-    // Clear token using the returned function 'clear' from the hook
-    try {
-      const userId = localStorage.getItem("userId"); // Retrieve the logged-in user's ID
-      if (!userId) return; // No ID? Skip request
-      // await fetch(`/users/${userId}/logout`, { method: "PUT" });
-      // const response = await apiService.put<void>(`/users/${userId}/logout`, {});
-      await apiService.put<void>(`/users/${userId}/logout`, {});
-    } catch (error) {
-      console.error("Logout failed:", error);
-    }
-
-    clearToken();
-    router.push("/login");
-  };
 
   useEffect(() => {
-    const storedToken = localStorage.getItem("token"); 
-    if(!storedToken){
-      router.push("/login");
-      return;
-    }
-    const fetchUsers = async () => {
-      
-      try {
-        // apiService.get<User[]> returns the parsed JSON object directly,
-        // thus we can simply assign it to our users variable.
-        const users: User[] = await apiService.get<User[]>("/users");
-        setUsers(users);
-        console.log("Fetched users:", users);
-      } catch (error) {
-        if (error instanceof Error) {
-          alert(`Something went wrong while fetching users:\n${error.message}`);
-        } else {
-          console.error("An unknown error occurred while fetching users.");
-        }
-      }
-    };
+    router.push("/dashboard");
+    return;
+  }, []);
 
-    fetchUsers();
-  }, [token, apiService, router]); // dependency apiService does not re-trigger the useEffect on every render because the hook uses memoization (check useApi.tsx in the hooks).
-  // if the dependency array is left empty, the useEffect will trigger exactly once
-  // if the dependency array is left away, the useEffect will run on every state change. Since we do a state change to users in the useEffect, this results in an infinite loop.
-  // read more here: https://react.dev/reference/react/useEffect#specifying-reactive-dependencies
+    return (
+      <div className={"exoplanet-nonExistant"}>
+        <Button
+          onClick={() => router.push("/dashboard")}
+          type="primary"
+          htmlType="button"
+        >
+          Please wait to be redirected to the dashboard.
+          If you are not automatically redirected, please click here
+        </Button>
+      </div>
+    );
+  };
 
-  return (
-    <div className={"card-container"}>
-      <Card
-        title="User overview:"
-        loading={!users}
-        className="dashboard-container"
-      >
-        {users && (
-          <>
-            {/* antd Table: pass the columns and data, plus a rowKey for stable row identity */}
-            <Table<User>
-              columns={columns}
-              dataSource={users}
-              rowKey="id"
-              onRow={(row) => ({
-                onClick: () => router.push(`/users/${row.id}`),
-                style: { cursor: "pointer" },
-              })}
-            />
-            <Button onClick={handleLogout} type="primary">
-              Logout
-            </Button>
-            <Button 
-            type="primary"
-            variant="solid"
-            onClick={()=> router.push("/profile")}>
-              My Profile
-
-            </Button>
-          </>
-        )}
-      </Card>
-    </div>
-  );
-};
-
-export default Dashboard;
+export default ExoplanetNonExistant;
