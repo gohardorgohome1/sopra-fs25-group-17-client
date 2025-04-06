765cf4064aae2172d09a6cc7f8f460050a021881
PascalSenn2 <pascal.senn2@uzh.ch>
2025-04-06 10:41:03 +0200

    resolved merge conflicts for squash & merge

--- app/exoplanets/upload/page.tsx
+++ app/exoplanets/upload/page.tsx
@@ -1,117 +1,292 @@
 "use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 import "@ant-design/v5-patch-for-react-19";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
-import { Button } from "antd";
-import { BookOutlined, CodeOutlined, GlobalOutlined } from "@ant-design/icons";
-import styles from "@/styles/page.module.css";
+import { useApi } from "@/hooks/useApi";
+import { Button, Form, Input, Card } from "antd";
 
-export default function Home() {
+const Upload: React.FC = () => {
   const router = useRouter();
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            <code>app/page.tsx</code> is the landing page for your application, currently being displayed.
-          </li>
-          <li>
-            <code>app/login/page.tsx</code> is the login page for users.
-          </li>
-          <li>
-            <code>app/users/page.tsx</code> is the dashboard that shows an overview of all users, fetched from the server.
-          </li>
-          <li>
-            <code>app/users/[id]/page.tsx</code> is a slug page that shows info of a particular user. Since each user has its own id, each user has its own infopage, dynamically with the use of slugs.
-          </li>
-          <li>
-            To test, modify the current page <code>app/page.tsx</code> and save to see your changes instantly.
-          </li>
-        </ol>
+  const apiService = useApi();
+  const [form] = Form.useForm();
 
-        <div className={styles.ctas}>
-          <Button
-            type="primary" // as defined in the ConfigProvider in [layout.tsx](./layout.tsx), all primary antd elements are colored #22426b, with buttons #75bd9d as override
-            color="red" // if a single/specific antd component needs yet a different color, it can be explicitly overridden in the component as shown here
-            variant="solid" // read more about the antd button and its options here: https://ant.design/components/button
-            onClick={() =>
-              globalThis.open(
-                "https://vercel.com/new",
-                "_blank",
-                "noopener,noreferrer",
-              )}
-            target="_blank"
-            rel="noopener noreferrer"
+  const handleUpload = async (value: { exoplanetName: string }) => {
+    try {
+      const response = await apiService.post<PhotometricCurve>("/upload",  value ); /////////////////////////////////////////////////////////
+
+      router.push("/dashboard");
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message.includes("Invalid photometric curve or exoplanet name!")) {
+          alert("Invalid photometric curve or exoplanet name!");
+        }
+        else{
+          alert(`Something went wrong during the upload of your photometric curve:\n${error.message}`);
+        }
+        
+      } else {
+        console.error("An unknown error occurred during the upload of your photometric curve!");
+      }
+    }
+  }
+
+  return (
+    <div
+        className="exoplanet-background"
+        style={{
+          position: "relative",
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+    >
+
+      <Card // Card for upload
+        className="uploadOuter-container"
+        style={{
+          width: "95vw", // size
+          height: "95vh",
+
+          background: "black", // visuals
+          border: "none",
+          borderRadius: 98,
+          backgroundColor: "rgba(0, 0, 0, 0.66)", // instead of opacity = 0.66 -> buttons etc. would inherit opacity
+
+          zIndex: 1 // foreground
+        }}
+      >
+        <div // Title: Exoplanet Transit Analysis
+          style={{
+            width: "50vw",
+            height: "12vh",
+            textAlign: 'center',
+            color: '#FFD9D9',
+            fontSize: 72,
+            fontFamily: 'Koulen',
+            fontWeight: '400',
+
+            background: "linear-gradient(90deg, #FFD9D9, #73CBC9)", // color gradient
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            }}
           >
-            Deploy now
-          </Button>
-          <Button
-            type="default"
-            variant="solid"
-            onClick={() =>
-              globalThis.open(
-                "https://nextjs.org/docs",
-                "_blank",
-                "noopener,noreferrer",
-              )}
-            target="_blank"
-            rel="noopener noreferrer"
+            Exoplanet Transit Analysis
+          </div>
+          
+          <div // This div makes sure the second card can be centered while the title is still at the top left
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              marginTop: "6vh"
+            }}
           >
-            Read our docs
-          </Button>
-          <Button
+            <Card
+              className="uploadInner-container"
+              style={{
+                width: "36vw", // size
+                height: "52vh",
+
+                display: "flex", // center items horizontally
+                flexDirection: "column",
+                alignItems: "center",
+
+                background: "black", // visuals
+                border: "none",
+                borderRadius: 26,
+                backgroundColor: "rgba(0, 0, 0, 0.66)", // instead of opacity = 0.66 -> buttons etc. would inherit opacity
+
+                zIndex: 1 // foreground
+              }}
+            >
+              <span
+                style=
+                {{
+                  display: "flex", // center text horizontally
+                  flexDirection: "column",
+                  alignItems: "center",
+
+                  textAlign: "center",
+                  color: "#8A5555",
+                  fontSize: "2vw",
+                  fontFamily: "Jura", // imported fontFamily -> see top of globals.css
+                  fontWeight: "700",
+
+                  background: "linear-gradient(90deg, #FFFFFF 0%, #D05C5C 63.9%, #B60000 100%)", // color gradient
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
+                }}
+              >
+                Upload a lightcurve file (.txt)
+              </span>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="upload-button"
+                style={{
+                  width: "32vw", // button size & style
+                  height: "8vh",
+                  background: "black",
+                  borderRadius: 46,
+                  marginTop: "20px",
+
+                  textAlign: "center", // Text size & style
+                  color: "#8A5555",
+                  fontSize: "40px",
+                  fontFamily: "Karantina", // imported fontFamily -> see top of globals.css
+                  fontWeight: "700",
+
+                  boxShadow: "none", // removes default green shadow of button
+                }}
+              >
+                <span
+                  style={{
+                    background: "linear-gradient(90deg, #8A5555, #FFFFFF)", // color gradient
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                    WebkitTextStrokeWidth: "1px",   // Add black edge to text
+                    WebkitTextStrokeColor: "#000000",
+                  }}
+                >
+                  Upload file
+                </span>
+              </Button>
+
+              <Form // Input field "Name of the Exoplanet" with label
+                form={form}
+                name="upload"
+                size="large"
+                variant="outlined"
+                onFinish={handleUpload}
+                layout="vertical" // Label on top by default
+                requiredMark={false} // no star before label
+              >
+                <Form.Item // Input exoplanet name field & Label
+                  name="exoplanetName"
+                  label={
+                  <span
+                    style={{
+                      width: "32vw", // size
+                      height: "8vh",
+                      marginTop: "6vh",
+
+                      textAlign: "center",
+                      background: "linear-gradient(90deg, #FFFFFF 0%, #D05C5C 63.9%, #B60000 100%)", // color gradient
+                      WebkitBackgroundClip: "text",
+                      WebkitTextFillColor: "transparent",
+                      fontSize: "2vw",
+                      fontFamily: "Jura", // imported fontFamily -> see top of globals.css
+                      fontWeight: "700",
+                    }}
+                  >
+                    Name of the Exoplanet
+                  </span>
+                  }
+
+                  rules={[{ required: true, message: "Please input the name of the Exoplanet!" }]}
+                >
+                  <Input
+                  style={{
+                    width: "32vw",
+                    height: "8vh",
+                    background: "white",
+                    borderRadius: 46,
+                    fontSize: "2vw",
+                    fontFamily: "Jura",
+                    color: "#000000"
+                  }}
+                  />
+                </Form.Item>
+                <Form.Item
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    gap: "2vh"
+                  }}
+                >
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{
+                      width: "32vw", // button size & style
+                      height: "6vh",
+                      background: "black",
+                      borderRadius: 46,
+                      //backdropFilter: "blur(10px)", // according to figma, we need this -> I don't think it is visible
+                      
+                      marginTop: "16vh", // button position
+
+                      textAlign: "center", // Text size & style
+                      color: "#8A5555",
+                      fontSize: "32px",
+                      fontFamily: "Karantina", // imported fontFamily -> see top of globals.css
+                      fontWeight: "700",
+
+                      boxShadow: '0px 0px 40px 12px rgba(255, 0, 0, 0.25)', // red glow around button
+                    }}
+                    >
+                    <span
+                      style={{
+                        background: "linear-gradient(90deg, #8A5555, #FFFFFF)", // color gradient
+                        WebkitBackgroundClip: "text",
+                        WebkitTextFillColor: "transparent",
+
+                        WebkitTextStrokeWidth: "1px",   // Add black edge to text
+                        WebkitTextStrokeColor: "#000000",
+                      }}
+                    >
+                      Calculate
+                    </span>
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Card>
+          </div>
+          
+          <Button // Button: Back to dashboard
+            onClick={() => router.push("/dashboard")}
             type="primary"
-            variant="solid"
-            onClick={() => router.push("/login")}
-          >
-            Go to login
+            htmlType="button"
+            style={{
+              width: "10vw", // button size & style
+              height: "6vh",
+              background: "black",
+              borderRadius: 46,
+              //backdropFilter: "blur(10px)", // according to figma, we need this -> I don't think it is visible
+              
+              marginLeft: "4vw", // button position
+              marginTop: "11vh",
+
+              textAlign: "center", // Text size & style
+              color: "#8A5555",
+              fontSize: "20px",
+              fontFamily: "Karantina", // imported fontFamily -> see top of globals.css
+              fontWeight: "700",
+
+              boxShadow: '0px 0px 40px 12px rgba(255, 0, 0, 0.25)', // red glow around button
+            }}
+            >
+            <span
+              style={{
+                background: "linear-gradient(90deg, #8A5555, #FFFFFF)", // color gradient
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+
+                WebkitTextStrokeWidth: "1px",   // Add black edge to text
+                WebkitTextStrokeColor: "#000000",
+              }}
+            >
+              Back to Dashboard
+            </span>
           </Button>
-          <Button
-            type="primary"
-            variant="solid"
-            onClick={() => router.push("/register")}
-          >
-            Register
-          </Button>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <Button
-          type="link"
-          icon={<BookOutlined />}
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </Button>
-        <Button
-          type="link"
-          icon={<CodeOutlined />}
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Examples
-        </Button>
-        <Button
-          type="link"
-          icon={<GlobalOutlined />}
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to nextjs.org →
-        </Button>
-      </footer>
+          
+      </Card>
+
     </div>
   );
 }
+
+export default Upload;
