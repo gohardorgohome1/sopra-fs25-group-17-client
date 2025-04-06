e6fae14aca461e8a015e88c9979aad37fe50f35a
PascalSenn2 <pascal.senn2@uzh.ch>
2025-04-06 10:27:18 +0200

    Changed upload-endpoint and some changes in login

--- app/exoplanets/upload/page.tsx
+++ app/exoplanets/upload/page.tsx
@@ -11,7 +11,7 @@ const Upload: React.FC = () => {
 
   const handleUpload = async (value: { exoplanetName: string }) => {
     try {
-      const response = await apiService.post<PhotometricCurve>("/exoplanet",  value ); /////////////////////////////////////////////////////////
+      const response = await apiService.post<PhotometricCurve>("/upload",  value ); /////////////////////////////////////////////////////////
 
       router.push("/dashboard");
     } catch (error) {
