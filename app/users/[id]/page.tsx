3eebec7c1857b067db23751b2ad717e236fc0086
PascalSenn2 <pascal.senn2@uzh.ch>
2025-05-07 18:02:25 +0200

    Fetching exoplanets of this user on his profile page #111

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -7,6 +7,12 @@ import { User } from "@/types/user";
 import { Button, Card, Form, Input, Modal } from "antd";
 import { useRouter } from "next/navigation";
 
+interface Exoplanet {
+    id: string;
+    planetName: string;
+    ownerId: string;
+  }
+
 const UserProfile = ({ params }: { params: Promise<{ id: string }> }) => {
     const { id } = React.use(params);
     const [user, setUser] = useState<User | null>(null);
@@ -16,6 +22,7 @@ const UserProfile = ({ params }: { params: Promise<{ id: string }> }) => {
     const [isCorrectUser, setIsCorrectUser] = useState(false);
     const [isEditing, setIsEditing] = useState(false);
     const [newUsername, setNewUsername] = useState(user?.username || "");
+    const [exoplanets, setExoplanets] = useState<Exoplanet[]>([]);
 
     useEffect(() => {
         const storedToken = localStorage.getItem("token");
@@ -27,20 +34,56 @@ const UserProfile = ({ params }: { params: Promise<{ id: string }> }) => {
         const storedId = localStorage.getItem("userId");
 
         const fetchUser = async () => {
-        try {
-            const user = await apiService.get<User>(`/users/${id}`);
-            setIsCorrectUser(user?.id === storedId) // Check if the current user is the user this profile page is from
-            setUser(user);
-            console.log("Fetched user:", user);
-            usernameForm.setFieldsValue({ username: user.username || "" });
-        } catch (error) {
-            if (error instanceof Error) {
-            alert(`Something went wrong while fetching user:\n${error.message}`);
-            } else {
-            console.error("An unknown error occurred while fetching user.");
+            try {
+                const user = await apiService.get<User>(`/users/${id}`);
+                setIsCorrectUser(user?.id === storedId) // Check if the current user is the user this profile page is from
+                setUser(user);
+                console.log("Fetched user:", user);
+
+                getMyExoplanets(user?.id); // Has to be called here to make sure it is executed AFTER the user is already fetched
+
+                usernameForm.setFieldsValue({ username: user.username || "" });
+            } catch (error) {
+                if (error instanceof Error) {
+                alert(`Something went wrong while fetching user:\n${error.message}`);
+                } else {
+                console.error("An unknown error occurred while fetching user.");
+                }
             }
-        }
         };
+
+        const fetchExoplanets = async (): Promise<Exoplanet[]> => {
+            try {
+              const planets = await apiService.get<Exoplanet[]>("/exoplanets");
+              console.log("Fetched exoplanets:", planets);
+              return planets;
+            } catch (error) {
+              if (error instanceof Error) {
+                alert(`Something went wrong while fetching the exoplanets:\n${error.message}`);
+                return [];
+              } else {
+                console.error("Unknown error while fetching the exoplanets.");
+                return [];
+              }
+            }
+        };
+
+        const getMyExoplanets = async (userId: string) => {
+            const exoplanets = await fetchExoplanets();
+            const myExoplanets: Exoplanet[] = [];
+            if (exoplanets.length === 0) {
+                setExoplanets([]);
+                return;
+            }
+            for (let i = 0; i < exoplanets.length; i++) {
+                const thisExoplanet = exoplanets[i];
+                const thisExoplanetOwner = thisExoplanet.ownerId;
+                if (thisExoplanetOwner == userId) {
+                    myExoplanets.push(thisExoplanet);
+                }
+            }
+            setExoplanets(myExoplanets);
+        }
 
         fetchUser();
     }, [id, usernameForm, apiService, router]);
@@ -217,6 +260,40 @@ const UserProfile = ({ params }: { params: Promise<{ id: string }> }) => {
                 >
                     created on
                 </div>
+
+                <Card
+                    style={{
+                        width: "92vw", // size
+                        height: "68vh",
+                        
+                        top: "14vh",
+                        position: "relative",
+            
+                        background: "black", // visuals
+                        border: "none",
+                        borderRadius: 98,
+                        backgroundColor: "rgba(0, 0, 0, 0.5)",
+            
+                        zIndex: 2 // foreground
+                }}
+                >
+                    <div
+                        style={{
+                            maxHeight: "100%",
+                            overflowY: "auto",
+                            padding: "2rem",
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "1rem",
+                          }}
+                        >
+                        {exoplanets.map((planet) => (
+                            <div key={planet.id} style={{ color: "white" }}>
+                                {planet.planetName} (Owner ID: {planet.ownerId})
+                            </div>
+                        ))}
+                    </div>
+                </Card>
 
                 <Button // Button: Back to dashboard
                     onClick={() => router.push("/dashboard")}
